c92ed88ef7ede0bf232c05d0a474067c30a96095
May Chen <maychen2580@hotmail.com>
2020-08-29 21:33:39 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    if end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|s| s.length < 2}.count
   end
 end
